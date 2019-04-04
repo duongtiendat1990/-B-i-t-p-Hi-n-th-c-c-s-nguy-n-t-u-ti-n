320ff486fc1607b14e4745a58d93f21ab2e83790
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-04-04 15:38:06 +0700

    "Hiển thị số nguyên tố"

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ function checkKey(e) {
         e.preventDefault()
     }
 }
+
+function checkNumberIsPrime(i, number) {
+    let isPrime = false;
+    for (i = 2; i <= number; i++) {
+        if (number % i === 0) {
+            if (i === number) {
+                isPrime = true;
+            } else break;
+        }
+    }
+    return isPrime;
+}
+
 function printPrimes() {
     let numberOfPrimes = Number(document.getElementById("numberOfPrimes").value);
     let count = 0;
@@ -17,23 +30,16 @@ function printPrimes() {
         default:
             let i, number;
             let j = 0;
-            for (number = 2; count < numberOfPrimes; number = number + 1) {
-                let isPrime = false;
-                for (i = 2; i <= number; i++) {
-                    if (number % i === 0) {
-                        if (i ===number) {
-                            isPrime = true;
-                        } else break;
-                    }
-                    if (isPrime) {
-                        count++;
-                        sout = sout + "<td>" + number + "</td>";
-                        j++;
-                        }
-                    if(j===10){
-                        sout = sout + "</ tr>" +"<tr>";
-                        j = 0
-                    }
+            for (number = 2; count < numberOfPrimes;number <=2 ? number ++ : number = number + 2) {
+                let isPrime = checkNumberIsPrime(i, number);
+                if (isPrime) {
+                    count++;
+                    sout = sout + "<td>" + number + "</td>";
+                    j++;
+                }
+                if(j===10){
+                    sout = sout + "</ tr>" +"<tr>";
+                    j = 0
                 }
             }
         break;
